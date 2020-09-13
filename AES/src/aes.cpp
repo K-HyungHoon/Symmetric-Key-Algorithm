57b9896472d76c2a5a8aa4e82c16c17b85df7813
axolotl0210 <axolotl0210@gmail.com>
2020-09-14 00:02:53 +0900

    Upload AES

---
+++ AES/src/aes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "AES.h"
+
+using namespace std;
+
+int main() {
+	AES aes;
+	// char plaintext[1025], key[17];
+
+	unsigned char plaintext[] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff };
+	unsigned char key[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+	int round = 10;
+
+	// cout << "Enter the Message to be encrypted: ";
+	// cin.getline(plaintext, sizeof(plaintext));
+
+	// cout << "Enter the Key: ";
+	// cin.getline(key, sizeof(key));
+
+	aes.encrypt(plaintext, key, round);
+
+	return 0;
+}
